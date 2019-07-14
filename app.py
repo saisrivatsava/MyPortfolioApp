1169a6eec8bbad1bc44a24a69b0f774db712d897
saisrivatsava.g <saisrivatsava.g@gmail.com>
2019-07-14 18:16:13 +0530

    added test File add logic

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from models import classification_models
 import pandas as pd
 import os
 from werkzeug.utils import secure_filename
+from flask import send_file
 
 
 UPLOAD_FOLDER = 'inputFiles'
+
+outputFileName= ""
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -46,19 +49,50 @@ def load_nimbus():
     target_feature=request.form['target_feature']
     inputState=request.form['inputState']
     data = pd.read_csv(file_url,encoding='latin-1')
-    if target_feature in data.columns:
-        classifier = classification_models.Classifier()
+    test_file_flag = request.form.get("outputDataFlag")
+
+    if test_file_flag !="yes":
+        test_file_flag="no"
+
+    classifier = classification_models.Classifier()
+
+    if test_file_flag=="no" and target_feature in data.columns:
         runDetails, result = classifier.train(data, target_feature)
         os.remove(file_url)
         return render_template('nimbus_output.html',result=result, runDetails=runDetails, pname=pname)
+
+    elif test_file_flag=="yes" and target_feature in data.columns:
+        test_file=request.files['test_file']
+        test_filename = secure_filename(test_file.filename)
+        test_file.save(os.path.join(app.config['UPLOAD_FOLDER'], test_filename))
+        test_file_url = os.path.join(app.config['UPLOAD_FOLDER'], test_filename)
+        target_features_to_include = request.form.get('target_features_to_include')
+        test_file_df = pd.read_csv(test_file_url,encoding='latin-1')
+
+        runDetails,sorted_scores_map, models_args_list, generatedFileName = classifier.trainWithTestData(data, test_file_df, target_feature,  target_features_to_include, pname)
+        global outputFileName
+        outputFileName = generatedFileName
+        print("opfile "+outputFileName)
+        os.remove(file_url)
+        return render_template('nimbus_output2.html', runDetails=runDetails,sorted_scores_map=sorted_scores_map, models_args_list=models_args_list, pname=pname)
+
     else:
         os.remove(file_url)
         return render_template('nimbus_error.html',target_feature=target_feature, pname=pname)
 
+    classifier.__del__(self)
 
 
+@app.route('/downloadPredicted') # this is a job for GET, not POST
+def download_csv():
+    print("inside download meth")
+    print(outputFileName)
+    return send_file(outputFileName,
+                     mimetype='text/csv',
+                     attachment_filename=outputFileName,
+                     as_attachment=True)
 
-
+# os.remove(outputFileName)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/classification_models.py
+++ models/classification_models.py
@@ -12,6 +12,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+import collections
 
 class Classifier:
     X_train = ""
@@ -19,6 +20,8 @@ class Classifier:
     y_train =""
     y_test =""
     raw_data = ""
+    X_raw=""
+    y_raw=""
 
     classifiers = [
     KNeighborsClassifier(3),
@@ -30,14 +33,63 @@ class Classifier:
     GradientBoostingClassifier()
     ]
 
+    classifiersMap = {
+    "KNeighborsClassifier":  KNeighborsClassifier(3),
+    "SVC":    SVC(kernel="rbf", C=0.025, probability=True),
+    "DecisionTreeClassifier":    DecisionTreeClassifier(),
+    "RandomForestClassifier":    RandomForestClassifier(),
+    "RandomForestClassifier":    AdaBoostClassifier(),
+    "GradientBoostingClassifier":    GradientBoostingClassifier()}
+
+    scoresMap={}
+    argsMap={}
+
     def train(self, data, target):
         self.loadAndSplitData(data, target)
-
         featuresList = self.getNumericAndCatFeatures(target)
         preprocessor = self.createPreprocessTransformer(featuresList)
         res  = self.fitModels(preprocessor)
         runDetails = self.getRunDetails()
         return (runDetails,res)
+
+    def trainWithTestData(self, train_data, test_data, target, col_names, runName):
+        self.loadAndSplitData(train_data, target)
+        # print("X_train "+str(len(self.X_train)))
+        # print("X_test "+str(len(self.X_test)))
+        featuresList = self.getNumericAndCatFeatures(target)
+        preprocessor = self.createPreprocessTransformer(featuresList)
+        sorted_scores_map  = self.fitModelsMap(preprocessor)
+        runDetails = self.getRunDetails()
+        sorted_scores_keys = sorted_scores_map.keys()
+        models_args_list = []
+        for modelName in sorted_scores_keys:
+            models_args_list.append(self.argsMap[modelName])
+
+        highest_scored_model = self.getHighestScoreModel(sorted_scores_map)
+        predicted_target = self.predictTarget(test_data,preprocessor, highest_scored_model)
+
+        default_cols_Df= test_data[col_names.split(",")]
+        predicted_Df = pd.DataFrame({target : predicted_target})
+        outputDf = pd.concat([default_cols_Df, predicted_Df], axis=1)
+        output_f_name=self.savePredictedFile(outputDf, runName)
+        return (runDetails,sorted_scores_map, models_args_list, output_f_name)
+
+
+    def savePredictedFile(self, outputDf, runName):
+        output_file_name = "OutputFiles/"+runName+"Prediction.csv"
+        outputDf.to_csv(output_file_name, index = False)
+        # filename = secure_filename(file.filename)
+        return output_file_name
+
+
+
+    def predictTarget(self, test_data,preprocessor, highest_scored_model):
+        pipe2 = Pipeline(steps=[('preprocessor', preprocessor),('classifier', highest_scored_model)])
+        model = highest_scored_model
+        pipe2.fit(self.X_raw, self.y_raw)
+
+        predictions = pipe2.predict(test_data)
+        return predictions
 
 
     def getRunDetails(self):
@@ -48,12 +100,18 @@ class Classifier:
 
         return resList
 
+    def getHighestScoreModel(self, sorted_map):
+        modelsList = list(sorted_map.keys())
+        model = self.classifiersMap[modelsList[0]]
+        return model
 
 
     def loadAndSplitData(self, data, target):
         self.raw_data = data
         X = self.raw_data.drop(target, axis=1)
         y = self.raw_data[target]
+        self.X_raw = X
+        self.y_raw = y
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2)
 
     def createPreprocessTransformer(self,featureList ):
@@ -96,3 +154,33 @@ class Classifier:
             # print(classifier)
             # print("model score: %.3f" % pipe.score(self.X_test, self.y_test))
         return result
+
+    def fitModelsMap(self, preprocessor):
+        scoresMap = {}
+
+        for name, cmodel in self.classifiersMap.items():
+            # print(name)
+            # print(model)
+            pipe = Pipeline(steps=[('preprocessor', preprocessor),('classifier', cmodel)])
+            pipe.fit(self.X_train, self.y_train)
+            # classifierName = type(classifier).__name__
+            score = pipe.score(self.X_test, self.y_test)
+            classifierArgs = str(cmodel.get_params())
+            scoresMap.update({name:score })
+            self.argsMap.update({name:classifierArgs})
+
+        sorted_scores = sorted(scoresMap.items(), key=lambda kv: kv[1], reverse=True)
+        sorted_scores_map = collections.OrderedDict(sorted_scores)
+        return sorted_scores_map
+            # # print(str(classifierArgs))
+            # resStr = str(i)+"|"
+            # i+=1
+            # resStr+=classifierName+"|"
+            # resStr+=classifierArgs+"|"
+            # resStr+=str(pipe.score(self.X_test, self.y_test))
+            # result.append(resStr)
+            # # print(classifier)
+            # print("model score: %.3f" % pipe.score(self.X_test, self.y_test))
+        # return result
+    def __del__(self):
+        print("Cleared Object")
